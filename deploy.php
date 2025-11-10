e13fe05046bdfd92e291f44d67cd0ead42e2866d
Duncan Pronk <info@dtpronk.nl>
2025-11-10 20:56:48 +0100

    feat: added deploy scripts

---
+++ deploy.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+set('application', 'dtpronk');
+set('repository', 'git@github.com:duncan412/dtpronk.nl.git');
+
+set('branch', 'main');
+
+set('composer_options', 'install --no-dev --no-interaction --prefer-dist --optimize-autoloader');
+
+add('shared_files', ['.env']);
+add('shared_dirs', ['storage']);
+
+set('writable_dirs', [
+    'bootstrap/cache',
+    'storage',
+    'storage/app',
+    'storage/framework',
+    'storage/logs',
+]);
+
+set('http_user', 'ploi');
+set('writable_mode', 'chmod');
+
+set('composer_self_update', false);
+
+host('production')
+    ->set('hostname', '46.62.246.246')
+    ->set('remote_user', 'ploi')
+    ->set('deploy_path', '/home/ploi/dtpronk.nl')
+    ->set('forward_agent', true)
+    ->set('labels', ['stage' => 'production']);
+
+task('deploy:optimize', [
+    'artisan:storage:link',
+    'artisan:view:cache',
+    'artisan:config:cache',
+]);
+
+task('build:assets', function () {
+    run('cd {{release_path}} && npm ci && npm run build');
+});
+
+desc('Deploy your Laravel app to Ploi.io');
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'build:assets',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:symlink',
+    'deploy:optimize',
+    // 'artisan:migrate',
+    'cleanup',
+]);
+
+after('deploy:failed', 'deploy:unlock');
+
+after('deploy:success', 'artisan:queue:restart');
