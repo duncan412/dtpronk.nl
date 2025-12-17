a8e22953fd1340676e3def0991e326d24206acd2
Duncan Pronk <info@dtpronk.nl>
2025-12-17 14:10:09 +0100

    fix: set up locale aware routes

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Page;
+use Illuminate\View\View;
 
 class PageController extends Controller
 {
-    public function index(string $slug)
+    public function index(string $slug): View
     {
-        app()->setLocale('nl');
+        $page = Page::whereJsonContainsLocale('slug', 'nl', $slug)
+            ->firstOrFail();
 
-        $page = Page::whereJsonContainsLocale('slug', app()->getLocale(), $slug)
+        return view('pages.index', compact('page'));
+    }
+
+    public function localeAware(string $locale, string $slug): View
+    {
+        $page = Page::whereJsonContainsLocale('slug', $locale, $slug)
             ->firstOrFail();
 
         return view('pages.index', compact('page'));
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Route::get('/', function () {
 });
 
 Route::get('/{slug}', [\App\Http\Controllers\PageController::class, 'index']);
+Route::get('/{locale}/{slug}', [\App\Http\Controllers\PageController::class, 'localeAware'])
+    ->where('locale', '[a-z]{2}');
